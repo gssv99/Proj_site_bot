074a0a841ef133d58ebe5d5339e2d20183543f75
Guilherme <guilherme_trabalho@outlook.com.br>
2024-12-02 15:04:36 -0400

    Feat: add templates pages and forms

--- proj_flask_bot/routes.py
+++ proj_flask_bot/routes.py
@@ -1,7 +1,32 @@
 from flask import render_template, url_for, redirect
-from fakepinterest import app, database, bcrypt
+from proj_flask_bot import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
-from fakepinterest.forms import FormLogin, FormCriarConta, FormFoto
-from fakepinterest.models import Usuario, Foto
+from proj_flask_bot.forms import FormLogin, FormCriarConta
+from proj_flask_bot.models import Usuario
 import os 
 from werkzeug.utils import secure_filename
+
+
+@app.route("/", methods = ["GET", "POST"])
+def homepage():
+    form_login = FormLogin()
+    if form_login.validate_on_submit():
+        usuario = Usuario.query.filter_by(email = form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=True)
+            return redirect(url_for("feed"))
+    return render_template("homepage.html", form = form_login)
+
+
+
+@app.route("/criarconta", methods = ["GET", "POST"])
+def criarconta():
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username= form_criarconta.username.data,senha=senha,email=form_criarconta.email.data)
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", id_usuario=usuario.id))
+    return render_template("criarconta.html", form = form_criarconta)
